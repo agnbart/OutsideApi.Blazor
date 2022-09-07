332aa58c4d54b03cc25b60f7105e331dae7beab2
PyLukasz <as.lukasz@gmail.com>
2022-09-07 18:47:38 +0200

    Added Project Data, EF, context class with base constructor.

---
+++ OutsideApi.Data/Persistance/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideApi.Data.Persistance
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
