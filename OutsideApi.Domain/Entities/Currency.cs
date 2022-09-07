ef4b40fcc9a1f0bd09c058b2f63385ff75a83839
PyLukasz <as.lukasz@gmail.com>
2022-09-07 18:58:10 +0200

    Add blazor page with exchange rates, nav button on page, currency entitie

---
+++ OutsideApi.Domain/Entities/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideApi.Domain.Entities
+{
+    public class Currency
+    {
+        public string? table { get; set; }
+        public string? currency { get; set; }
+        public string? code { get; set; }
+        public Rate[]? rates { get; set; }
+    }
+    public class Rate
+    {
+        public string? no { get; set; }
+        public string? effectiveDate { get; set; }
+        public float mid { get; set; }
+    }
+}
