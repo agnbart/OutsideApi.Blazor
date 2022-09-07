6a941ecc5906dd428c97d0492e5493d890214c4a
PyLukasz <as.lukasz@gmail.com>
2022-09-07 19:43:35 +0200

    Added connection string.

--- OutsideApi.Data/Persistance/Context.cs
+++ OutsideApi.Data/Persistance/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OutsideApi.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,15 @@ namespace OutsideApi.Data.Persistance
 {
     public class Context : DbContext
     {
+        private string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=ExternalAPI;Trusted_Connection=True;";
         public Context(DbContextOptions<Context> options) : base(options)
         {
 
+        }
+        public DbSet<Currency> MyProperty { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
         }
     }
 }
